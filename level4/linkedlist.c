8bc4fbf870cdff09893c46212ca73bf64fd1d2da
surajbartoula <suraj_bartoula@yahoo.com>
2024-02-22 07:18:38 +0000

    done

--- level4/linkedlist.c
+++ level4/linkedlist.c
@@ -19,20 +19,20 @@ int main()
 {
 	struct Node *head;
 	struct Node *second;
-	struct node *third;
+	struct Node *third;
 
 	head = (struct Node*)malloc(sizeof(struct Node));
 	second = (struct Node*)malloc(sizeof(struct Node));
 	third = (struct Node*)malloc(sizeof(struct Node));
 
-	head.data = 7;
-	head.next = second;
+	head->data = 7;
+	head->next = second;
 
-	second.data = 11;
-	second.next = third;
+	second->data = 11;
+	second->next = third;
 
-	third.data = 66;
-	third.next = NULL;
+	third->data = 66;
+	third->next = NULL;
 	
 	return (0);
 }
